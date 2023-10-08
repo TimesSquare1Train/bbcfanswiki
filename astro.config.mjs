9360a97f4058fd67b796a64423330dc032d38acf
valbuilds <katie@katiebuilds.website>
2023-10-08 07:47:13 -0500

    reith

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,18 +8,21 @@ export default defineConfig({
 		starlight({
 			title: 'BBC Fans Wiki',
 			customCss: [
-				'./src/bbcfans.css'
+				'./src/bbcfans.css',
+				'./src/fonts/reith.css'
 			],
 			social: {
 				discord: 'https://discord.gg/PMQEnTxZxa',
 				github: 'https://github.com/valbuilds/bbcfanswiki'
 			},
 			sidebar: [
+				{ label: 'Explore', link: '/wiki/explore' },
 				{
 					label: 'Programmes',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'All Programmes', link: '/wiki/programmes' },
+						{ label: 'BBC Breakfast', link: '/wiki/programme/bbc-breakfast' },
 					],
 				},
 				{
